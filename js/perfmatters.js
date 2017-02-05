7ae1db652117af0b3e47f8bd883fec4c9dba9074
LAPTOP-HMDMPT5G\Katerina <t.ekaterina.y@gmail.com>
2017-02-05 04:17:55 +0300

    googles wishes

--- js/perfmatters.js
+++
@@ -1,4 +0,0 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
-function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,c=a.domComplete-a.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(a){logCRP()});
